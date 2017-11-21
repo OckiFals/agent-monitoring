d1dc93787533982a2a84268d65c51c6419c9ba33
Ocki Bagus Pratama <ocki.bagus.p@gmail.com>
2017-11-21 15:23:32 +0700

    initial

---
+++ agent_dev.py
@@ -0,0 +1,146 @@
+from twisted.internet.protocol import DatagramProtocol
+from twisted.internet import reactor, threads
+import time,psutil,os,json,socket
+from uuid import getnode as get_mac
+
+class ClientProtocol(DatagramProtocol):
+    def startProtocol(self):
+        print 'client started'
+        host = "127.0.0.1"
+        self.transport.connect('127.0.0.1', 9982)
+        mac = getmac()
+        hostname=gethostname()
+        response = os.system("ping -c 1 " + host)
+        if response == 0:
+            avai = "Up"
+        else:
+            avai = "Down"
+        status = (hostname,mac, avai ,"Off")
+        result = json.dumps(status)
+        self.transport.write(result)
+    def doStop(self):
+        KeyboardInterrupt
+        hostname = gethostname()
+        mac = getmac()
+        status = (hostname,mac, "Down")
+        result = json.dumps(status)
+        self.transport.write(result)
+        print "Client Stopped"
+    def stopProtocol(self):
+        print 'client stopped'
+
+    def datagramReceived(self, datagram, (host,port)):
+        command = json.loads(datagram)
+        print 'Datagram received: ', repr(command)
+        handler = Handler(self.transport,host,port)
+        d = threads.deferToThread(handler.handleMessage, command)
+        duration=((command[5] - command[4])/command[3])
+
+        for i in range(duration):
+            if command[2] == 1:
+                d.addCallback(handler.sendRes)
+            elif command[2] == 2:
+                d.addCallback(handler.sendNetwork)
+                #time.sleep(command[3])
+            elif command[2] == 3:
+                d.addCallback(handler.sendPing)
+            elif command[2] == 4:
+                #for i in range(duration):
+                d.addCallback(handler.sendDisk)
+                #print command[3]
+                    #time.sleep(command[3])
+            print "data sent"
+            time.sleep(command[3])
+
+            #print command[3]
+
+
+def getmac():
+    mac = get_mac()
+    macstring=':'.join(("%012X" % mac)[i:i+2] for i in range(0, 12, 2))
+    return macstring
+def gethostname():
+    hostname=socket.gethostname()
+    return hostname
+
+def ping():
+    type =3
+    host = "127.0.0.1"
+    response = os.system("ping -c 1 " + host)
+    if response == 0:
+        avai="Device is up"
+    else:
+        avai="Device is down"
+    status = (type,getmac(),host,avai)
+    result =json.dumps(status)
+    return result
+
+def getNetwork():
+    type=2
+    bytesent = psutil.net_io_counters().bytes_sent
+    byterecv = psutil.net_io_counters().bytes_recv
+    pktsent = psutil.net_io_counters().packets_sent
+    pktrcv = psutil.net_io_counters().packets_recv
+
+    net = (type,getmac(),bytesent, byterecv, pktsent, pktrcv)
+    result = json.dumps(net)
+
+    return result
+def getResource():
+    type=1
+    cpu = psutil.cpu_percent(interval=1)
+    memory = psutil.virtual_memory().available
+    usedmem = psutil.virtual_memory().used
+    swap = psutil.swap_memory().free
+    res = (type,getmac(),cpu, memory, usedmem, swap)
+    result = json.dumps(res)
+    return result
+
+def getDisk():
+    type=4
+    disk_used = (psutil.disk_usage('/')[1]) / 1024
+    disk_free = (psutil.disk_usage('/')[2]) / 1024
+    read_bytes = psutil.disk_io_counters(perdisk=False)[2] / 1024
+    write_bytes = psutil.disk_io_counters(perdisk=False)[3] / 1024
+    disk=(type,getmac(),disk_used,disk_free,read_bytes,write_bytes)
+    result=json.dumps(disk)
+    return result
+
+class Handler():
+    def __init__(self, transport, host, port):
+        self.host = host
+        self.port = port
+        self.transport = transport
+
+    def handleMessage(self, command):
+        print 'Handler Message: ', repr(command)
+        #print command[3]
+        #return command[3]
+    def sendRes(self, *arg):
+        #handler = Handler()
+        #interval=handler.handleMessage(self.transport,host,port)
+        #time.sleep(10)
+        #print type(getResource())
+        self.transport.write(getResource())
+    def sendNetwork(self, *arg):
+        #time.sleep(10)
+        #print type(getNetwork())
+        self.transport.write(getNetwork())
+    def sendPing(self, *arg):
+        #time.sleep(10)
+        self.transport.write(ping())
+    def sendDisk(self, *arg):
+        #time.sleep(10)
+        #print type(getDisk())
+        self.transport.write(getDisk())
+    def postHandle(self, arg):
+        print 'post handling'
+
+def main():
+
+    protocol = ClientProtocol()
+    reactor.listenUDP(0, protocol)
+    reactor.run()
+
+if __name__ == '__main__':
+    main()
---
+++ monit-app-iot/app/models.py
@@ -0,0 +1,61 @@
+from app import db
+
+class Host(db.Model):
+    id = db.Column(db.Integer, index=True, unique=True)
+    hostname=db.Column(db.String(64))
+    host = db.Column(db.String(64), primary_key=True)
+    status = db.Column(db.String(64))
+    phase=db.Column(db.String(64))
+
+class Resource(db.Model):
+    id = db.Column(db.Integer, primary_key=True, index=True, unique=True)
+    host = db.Column(db.String(64))
+    date = db.Column(db.String(64))
+    time = db.Column(db.String(64))
+    cpu = db.Column(db.Integer)
+    memory_avail = db.Column(db.Integer)
+    memory_used = db.Column(db.Integer)
+    swap_free = db.Column(db.Integer)
+
+class Network(db.Model):
+    id = db.Column(db.Integer, primary_key=True, index=True, unique=True)
+    host = db.Column(db.String(64))
+    date = db.Column(db.String(64))
+    time = db.Column(db.String(64))
+    byte_sent = db.Column(db.Integer)
+    byte_receive = db.Column(db.Integer)
+    packet_sent = db.Column(db.Integer)
+    packet_receive = db.Column(db.Integer)
+
+class Disk(db.Model):
+    id = db.Column(db.Integer, primary_key=True, index=True, unique=True)
+    host = db.Column(db.String(64))
+    date = db.Column(db.String(64))
+    time = db.Column(db.String(64))
+    disk_used = db.Column(db.Integer)
+    disk_free = db.Column(db.Integer)
+    read_bytes = db.Column(db.Integer)
+    write_bytes = db.Column(db.Integer)
+
+class Availability(db.Model):
+    id = db.Column(db.Integer, primary_key=True, index=True, unique=True)
+    host = db.Column(db.String(64))
+    date = db.Column(db.String(64))
+    time = db.Column(db.String(64))
+    status = db.Column(db.String(64))
+
+class Monitor(db.Model):
+    id = db.Column(db.Integer, primary_key=True, index=True, unique=True)
+    host = db.Column(db.String(64))
+    monitype = db.Column(db.Integer)
+    interval = db.Column(db.Integer)
+    start = db.Column(db.Integer)
+    end = db.Column(db.Integer)
+    createdat = db.Column(db.String(64))
+    def __init__(self, host, monitype, interval, start, end, createdat) :
+        self.host=host
+        self.monitype=monitype
+        self.interval=interval
+        self.start=start
+        self.end=end
+        self.createdat=createdat
